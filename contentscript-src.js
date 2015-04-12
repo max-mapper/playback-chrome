3cc51d9d89ad6ed4f534c6447da780befe000d1a
Max Ogden <max@maxogden.com>
2015-04-12 13:56:51 -0700

    hopefully fix href bug

--- contentscript-src.js
+++ contentscript-src.js
@@ -1,17 +1,32 @@
 var newElement = require('new-element')
 var fs = require('fs')
+var domready = require('domready')
 var template = fs.readFileSync('./button.html').toString()
 
-var buttons = document.querySelector('.html5-player-chrome')
-var button = newElement(template)
-var href = window.location.href
-var parts = href.split(':')
-parts.shift() // get rid of https
-parts.unshift('playback')
-button.href = parts.join(':')
-buttons.insertBefore(button, buttons.firstChild)
+domready(inject)
 
-button.addEventListener('click', function () {
-  var pause = buttons.querySelector('.ytp-button-pause')
-  if (pause) pause.click()
-})
+function inject () {
+  var buttons = document.querySelector('.html5-player-chrome')
+  var button = newElement(template)
+  
+  button.href = getHref()
+  buttons.insertBefore(button, buttons.firstChild)
+
+  button.addEventListener('click', function () {
+    var pause = buttons.querySelector('.ytp-button-pause')
+    if (pause) pause.click()
+  })
+  
+  // hack if wrong href got set at inject time
+  button.addEventListener('mouseover', function () {
+    button.href = getHref()
+  }) 
+}
+
+function getHref() {
+  var href = window.location.href
+  var parts = href.split(':')
+  parts.shift() // get rid of https
+  parts.unshift('playback')
+  return parts.join(':')
+}
