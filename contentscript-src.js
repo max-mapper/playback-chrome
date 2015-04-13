72723f5bae8a8470bcc32f120c7a9ab46ff120b8
Max Ogden <max@maxogden.com>
2015-04-13 10:32:36 -0700

    fix for new youtube player. closes #2

--- contentscript-src.js
+++ contentscript-src.js
@@ -2,18 +2,32 @@ var newElement = require('new-element')
 var fs = require('fs')
 var domready = require('domready')
 var template = fs.readFileSync('./button.html').toString()
+var newTemplate = fs.readFileSync('./new-button.html').toString()
 
+var tries = 0
 domready(inject)
+
 
 function inject () {
   var buttons = document.querySelector('.html5-player-chrome')
   var button = newElement(template)
+  
+  if (!buttons) {
+    buttons = document.querySelector('.ytp-chrome-controls')
+    button = newElement(newTemplate)
+  }
+  
+  if (!buttons) {
+    if (++tries > 5) return
+    setTimeout(inject, 1000)
+  }
   
   button.href = getHref()
   buttons.insertBefore(button, buttons.firstChild)
 
   button.addEventListener('click', function () {
     var pause = buttons.querySelector('.ytp-button-pause')
+    if (!pause) pause = buttons.querySelector('.ytp-play-button')
     if (pause) pause.click()
   })
   
